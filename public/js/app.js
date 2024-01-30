c9d87a450346100ea9d461ddeb2320dfb55404ec
Luis Pardo <luispardosuarez@gmail.com>
2024-01-30 22:59:58 +0100

    change app.js code and add css style

--- public/js/app.js
+++ public/js/app.js
@@ -1,59 +1,30 @@
-
-const uri = '../data.json'
+const uri = '../data.json';
+let table = document.getElementById('tableAll');
 
 fetch(uri)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
+      
+        let json = data.results; 
+       
+        table.innerHTML = '';
+
+        json.forEach(function(item) {
+            let row = table.insertRow();
+    
+            let nameCell = row.insertCell();
+            nameCell.appendChild(document.createTextNode(item.title))
+    
+            let propiety1Cell = row.insertCell();
+            propiety1Cell.appendChild(document.createTextNode(item.priority))
+    
+            let propiety2Cell = row.insertCell();
+            propiety2Cell.appendChild(document.createTextNode(item.isDone))
+        });
     })
+
     .catch(function(error){
-        console.log('Error load Json file'. error)
-    })
-
-
-
-
-let table = document.getElementById('tableAll');
-
-let json = [{
-        "title": "Estudiar JS",
-        "priority": "alta",
-        "isDone": "false"
-    },
-    {
-        "title": "Estudiar CSS",
-        "priority": "alta",
-        "isDone": "true"
-
-    },
-    {
-        "title": "Estudiar OPP",
-        "priority": "media",
-        "isDone": "false"
-
-    },
-    {
-        "title": "Estudiar IA",
-        "priority": "baja",
-        "isDone": "false"
-
-    },
-
-
-];
-
-for (let i = 0; i < json.length; i++) {
-    let row = table.insertRow();
-
-    let nameCell = row.insertCell();
-    nameCell.appendChild(document.createTextNode(json[i].title))
-
-    let propiety1Cell = row.insertCell();
-    propiety1Cell.appendChild(document.createTextNode(json[i].priority))
-
-    let propiety2Cell = row.insertCell();
-    propiety2Cell.appendChild(document.createTextNode(json[i].isDone))
-
-}
+        console.log('Error cargando el archivo JSON:', error);
+    });
